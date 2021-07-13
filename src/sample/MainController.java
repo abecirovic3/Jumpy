7e40dc5a36633f417fcc54dfb0f5589bbe5b7cfc
abecirovic3 <abecirovic3@email.com>
2021-07-13 13:43:43 +0200

    Added game screen

---
+++ src/sample/MainController.java
@@ -0,0 +1,37 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
+public class MainController {
+    public void startGameAction(ActionEvent actionEvent) {
+        GameController ctrl = new GameController();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/game.fxml"));
+        loader.setController(ctrl);
+
+        Parent root = null;
+        try {
+            root = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage stage = new Stage();
+        stage.setTitle("Jumpy");
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.setResizable(false);
+        stage.show();
+
+        Node node = (Node) actionEvent.getSource();
+        Stage currStage = (Stage) node.getScene().getWindow();
+        currStage.close();
+    }
+}
