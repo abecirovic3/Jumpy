e0fa7c18658b6c1d35e1583bd90d649de83d84d5
abecirovic3 <abecirovic3@email.com>
2021-07-13 15:16:14 +0200

    OK game layout

--- src/sample/GameController.java
+++ src/sample/GameController.java
@@ -1,4 +1,12 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
 public class GameController {
+
+    @FXML
+    public void initialize() {
+    }
 }
