c720de26be4cde82ce4eeef8025119f9849edf99
abecirovic3 <abecirovic3@email.com>
2021-07-30 14:26:32 +0200

    end alert name field validation done

--- src/sample/EndAlertController.java
+++ src/sample/EndAlertController.java
@@ -87,6 +87,10 @@ public class EndAlertController {
     }
 
     public void closeAlertAction(ActionEvent actionEvent) {
+        if (nameField.getText().equals("")) {
+            nameField.setStyle("-fx-border-color: red;");
+            return;
+        }
         model.dao.addHighscore(nameField.getText(), model.stopwatch.getElapsedTime().toString(), model.difficulty.toString());
         Stage currStage = (Stage) labelTitleMessage.getScene().getWindow();
         currStage.close();
