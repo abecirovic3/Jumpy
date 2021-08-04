b9f002a3d358584b5fbba4dbb670f82233e5f21a
abecirovic3 <abecirovic3@email.com>
2021-08-04 11:58:24 +0200

    basic layout of Helo view done

--- src/sample/HelpController.java
+++ src/sample/HelpController.java
@@ -5,7 +5,7 @@ import javafx.scene.Node;
 import javafx.stage.Stage;
 
 public class HelpController {
-    
+
     public void closeAction(ActionEvent actionEvent) {
         Node node = (Node) actionEvent.getSource();
         Stage currStage = (Stage) node.getScene().getWindow();
